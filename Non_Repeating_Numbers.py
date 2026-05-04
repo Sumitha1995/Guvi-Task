010f6a5774880fdf4fbfb87333f820825e607c54
Sumitha1995 <v.smitha2013@gmail.com>
2026-05-04 11:58:59 +0530

    Delete Non_Repeating_Numbers.py

--- Non_Repeating_Numbers.py
+++
@@ -1,9 +0,0 @@
-lis1 = [1,2,3,5,6,5,5] #list of numbers
-
-for i in lis1: #accessing numbers in the list
-
-    if lis1.count(i) == 1: # Check current element appears only once in the list
-
-        print("First non-repeating element:", i)
-
-        break
