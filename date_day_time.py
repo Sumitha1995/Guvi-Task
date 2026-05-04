847281118c0ec4b89bb8c17ed68767e8ab6c703d
Sumitha1995 <v.smitha2013@gmail.com>
2026-05-04 17:25:56 +0530

    Add files via upload
    Fifth Program

---
+++ date_day_time.py
@@ -0,0 +1,7 @@
+from datetime import datetime   # datetime class is imported from datetime module
+
+d = datetime(2022, 12, 16)   # create a datetime object with year, month, day
+
+print("Year:", d.year)    # extract and print year
+print("Month:", d.month)  # extract and print month
+print("Day:", d.day)	   # extract and print day
