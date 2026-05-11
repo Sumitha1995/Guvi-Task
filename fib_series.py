47627b0e646976ed4fc65d281636533da89a5e7c
Sumitha1995 <v.smitha2013@gmail.com>
2026-05-11 19:38:16 +0530

    Delete fib_series.py

--- fib_series.py
+++
@@ -1,9 +0,0 @@
-n = 5  #number of terms to print
-
-a, b = 0, 1 #first two fibonacci numbers
-
-fib = lambda x, y: (y, x + y) #lambda function to generate next pair
-
-for i in range(n):
-    print(a, end=" ") #Prints current fibonacci series
-    a, b = fib(a, b) #update value
