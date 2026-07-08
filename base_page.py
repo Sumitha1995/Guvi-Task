88f85ef6eb464671939156b88490a437205df643
Sumitha1995 <v.smitha2013@gmail.com>
2026-07-08 09:15:19 +0530

    Add files via upload
    Task 14 - POM

---
+++ base_page.py
@@ -0,0 +1,32 @@
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+class BasePage:
+
+    def __init__(self, driver):
+        self.driver = driver
+        self.wait = WebDriverWait(driver, 10)
+
+    def open_url(self, url):
+        self.driver.get(url)
+
+    def enter_text(self, locator, value):
+        element = self.wait.until(EC.visibility_of_element_located(locator))
+        element.clear()
+        element.send_keys(value)
+
+    def click_element(self, locator):
+        self.wait.until(EC.element_to_be_clickable(locator)).click()
+
+    def get_text(self, locator):
+        return self.wait.until(EC.visibility_of_element_located(locator)).text
+
+    def get_title(self):
+        return self.driver.title
+
+    def get_current_url(self):
+        return self.driver.current_url
+
+    def close_browser(self):
+        self.driver.quit()
